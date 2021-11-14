0e6aa125e0fc8c9d27fdf00c60ac334d1b340e77
s22215 <81318715+s22215@users.noreply.github.com>
2021-11-14 12:39:57 +0100

    Create voice model.

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
    author='Michal Pilichowski',
    author_email='',
    packages=['pilichm.gameObjects'],
-   install_requires=['Pillow', 'IPython', 'openfst-python']
+   install_requires=['Pillow', 'IPython', 'openfst-python', 'lexicon']
 )
