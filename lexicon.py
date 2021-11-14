81dc5c2b0d6cd3c30b683db41628aa873fc79b95
s22215 <81318715+s22215@users.noreply.github.com>
2021-11-14 13:46:37 +0100

    Create voice model.

--- lexicon.py
+++
@@ -1,2 +0,0 @@
-def words_to_lexicon(wordlist, model):
-    pass
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='VoiceCOntrolledGame',
+   version='1.0',
+   description='',
+   author='Michal Pilichowski',
+   author_email='',
+   packages=['pilichm.gameObjects'],
+   install_requires=['Pillow', 'IPython']
+)
