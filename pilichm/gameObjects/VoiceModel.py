fc6f476b80d7c989902aba2514e3d47a8c0c5f14
s22215 <81318715+s22215@users.noreply.github.com>
2021-11-16 16:39:08 +0100

    Method for removing leading and trailing silence.

--- pilichm/gameObjects/VoiceModel.py
+++ pilichm/gameObjects/VoiceModel.py
@@ -5,7 +5,7 @@ import re
 from pilichm.gameObjects.Direction import *
 from pilichm.gameObjects.Constants import PATH_TO_GRAMMAR, RECORDING_FILENAME, RESOURCES_DIR, PATH_TO_MODEL_CONF_FILE
 
-wordlist = ['w', 'prawo', 'lewo', 'dół', 'góra']
+wordlist = ['prawo', 'lewo', 'dół', 'góra']
 
 
 def add_arc(sf, st, word, wsyms, g):
@@ -92,14 +92,32 @@ class VoiceModel:
         s0 = self.grammar.add_state()
         s1 = self.grammar.add_state()
         s2 = self.grammar.add_state()
+        s3 = self.grammar.add_state()
+        s4 = self.grammar.add_state()
 
-        self.grammar = add_arc(s0, s0, 'prawo', wsyms, self.grammar)
-        self.grammar = add_arc(s0, s0, 'lewo', wsyms, self.grammar)
-        self.grammar = add_arc(s0, s0, 'góra', wsyms, self.grammar)
+        self.grammar = add_arc(s0, s1, 'prawo', wsyms, self.grammar)
+        self.grammar = add_arc(s0, s2, 'lewo', wsyms, self.grammar)
+        self.grammar = add_arc(s0, s3, 'góra', wsyms, self.grammar)
+        self.grammar = add_arc(s1, s4, 'dół', wsyms, self.grammar)
 
-        self.grammar = add_arc(s0, s1, 'w', wsyms, self.grammar)
-        self.grammar = add_arc(s1, s0, 'dół', wsyms, self.grammar)
+        s5 = self.grammar.add_state()
+        s6 = self.grammar.add_state()
+        s7 = self.grammar.add_state()
+        s8 = self.grammar.add_state()
+
+        self.grammar = add_arc(s1, s5, 'prawo', wsyms, self.grammar)
+        self.grammar = add_arc(s2, s6, 'lewo', wsyms, self.grammar)
+        self.grammar = add_arc(s3, s7, 'góra', wsyms, self.grammar)
+        self.grammar = add_arc(s4, s8, 'dół', wsyms, self.grammar)
+
+        s9 = self.grammar.add_state()
+
+        self.grammar = add_arc(s5, s9, 'prawo', wsyms, self.grammar)
+        self.grammar = add_arc(s6, s9, 'lewo', wsyms, self.grammar)
+        self.grammar = add_arc(s7, s9, 'góra', wsyms, self.grammar)
+        self.grammar = add_arc(s8, s9, 'dół', wsyms, self.grammar)
 
         self.grammar.set_start(s0)
-        self.grammar.set_final(s0)
+        self.grammar.set_final(s9)
+
         self.grammar = fst.determinize(self.grammar.rmepsilon())
