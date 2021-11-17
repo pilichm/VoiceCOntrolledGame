32b29afc8dbb1ccb756e108769336c6fca11aef7
s22215 <81318715+s22215@users.noreply.github.com>
2021-11-17 17:54:24 +0100

    Method for removing leading and trailing silence.

--- pilichm/gameObjects/VoiceModel.py
+++ pilichm/gameObjects/VoiceModel.py
@@ -5,7 +5,7 @@ import re
 from pilichm.gameObjects.Direction import *
 from pilichm.gameObjects.Constants import PATH_TO_GRAMMAR, RECORDING_FILENAME, RESOURCES_DIR, PATH_TO_MODEL_CONF_FILE
 
-wordlist = ['zaklęcie', 'kula', 'ognia', 'leczenie', 'wykonaj', 'kolejny', 'ruch', 'podnieś', 'przedmiot']
+wordlist = ['zaklęcie', 'kula', 'ognia', 'leczenie', 'podnieś', 'przedmiot', 'do', 'w', 'prawo', 'lewo', 'góry', 'dół']
 
 
 def add_arc(sf, st, word, wsyms, g):
@@ -89,31 +89,35 @@ class VoiceModel:
         self.grammar.set_input_symbols(wsyms)
         self.grammar.set_output_symbols(wsyms)
 
-        wordlist = ['zaklęcie', 'kula', 'ognia', 'leczenie', 'wykonaj', 'kolejny', 'ruch', 'podnieś', 'przedmiot']
+        wordlist = ['zaklęcie', 'kula', 'ognia', 'leczenie', 'podnieś', 'przedmiot', 'do', 'w', 'prawo', 'lewo', 'góry', 'dół']
 
         s0 = self.grammar.add_state()
+
         s1 = self.grammar.add_state()
         s2 = self.grammar.add_state()
         s3 = self.grammar.add_state()
-
         s4 = self.grammar.add_state()
+
         s5 = self.grammar.add_state()
         s6 = self.grammar.add_state()
 
         self.grammar = add_arc(s0, s1, 'zaklęcie', wsyms, self.grammar)
-        self.grammar = add_arc(s0, s2, 'wykonaj', wsyms, self.grammar)
-        self.grammar = add_arc(s0, s3, 'podnieś', wsyms, self.grammar)
+        self.grammar = add_arc(s0, s2, 'podnieś', wsyms, self.grammar)
+        self.grammar = add_arc(s0, s3, 'do', wsyms, self.grammar)
+        self.grammar = add_arc(s0, s4, 'w', wsyms, self.grammar)
 
-        self.grammar = add_arc(s1, s4, 'kula', wsyms, self.grammar)
-        self.grammar = add_arc(s1, s5, 'leczenie', wsyms, self.grammar)
-        self.grammar = add_arc(s4, s5, 'ognia', wsyms, self.grammar)
+        self.grammar = add_arc(s1, s5, 'kula', wsyms, self.grammar)
+        self.grammar = add_arc(s5, s6, 'ognia', wsyms, self.grammar)
 
-        self.grammar = add_arc(s2, s6, 'kolejny', wsyms, self.grammar)
-        self.grammar = add_arc(s6, s5, 'ruch', wsyms, self.grammar)
+        self.grammar = add_arc(s1, s6, 'leczenie', wsyms, self.grammar)
 
-        self.grammar = add_arc(s3, s5, 'przedmiot', wsyms, self.grammar)
+        self.grammar = add_arc(s3, s6, 'góry', wsyms, self.grammar)
+        self.grammar = add_arc(s3, s6, 'dołu', wsyms, self.grammar)
+
+        self.grammar = add_arc(s4, s6, 'prawo', wsyms, self.grammar)
+        self.grammar = add_arc(s4, s6, 'lewo', wsyms, self.grammar)
 
         self.grammar.set_start(s0)
-        self.grammar.set_final(s5)
+        self.grammar.set_final(s6)
 
         self.grammar = fst.determinize(self.grammar.rmepsilon())
